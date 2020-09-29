ec3a799b7f8f4d94ddf2f1f6a86fda0921af897c
Czino <eric@czino.de>
2020-09-29 17:51:24 +0200

    Create forest tileset

--- src/mapUtils/maps/forest.js
+++ src/mapUtils/maps/forest.js
@@ -8,9 +8,9 @@ import constants from '../../constants'
 
 const tileSize = 8
 const t00 = [0, 0], t01 = [0, 1], t02 = [0, 2], t03 = [0, 3], t04 = [0, 4], t05 = [0, 5], t06 = [0, 6], t07 = [0, 7], t08 = [0, 8], t010 = [0, 10],
-  t10 = [1, 0], t11 = [1, 1], t15 = [1, 5], t18 = [1, 8], t17 = [1, 7], t19 = [1, 9], t110 = [1, 10],
-  t20 = [2, 0], t21 = [2, 1], t22 = [2, 2], t24 = [2, 4], t25 = [2, 5], t27 = [2, 7], t28 = [2, 8], t210 = [2, 10],
-  t31 = [3, 1], t32 = [3, 2], t34 = [3, 4], t37 = [3, 7], t310 = [3, 10],
+  t10 = [1, 0], t11 = [1, 1], t14 = [1, 4], t15 = [1, 5], t18 = [1, 8], t17 = [1, 7], t19 = [1, 9], t110 = [1, 10],
+  t20 = [2, 0], t21 = [2, 1], t22 = [2, 2], t24 = [2, 4], t25 = [2, 5], t26 = [2, 6], t27 = [2, 7], t28 = [2, 8], t210 = [2, 10],
+  t31 = [3, 1], t32 = [3, 2], t34 = [3, 4], t36 = [3, 6], t37 = [3, 7], t310 = [3, 10],
   t41 = [4, 1], t43 = [4, 3], t44 = [4, 4], t45 = [4, 5], t47 = [4, 7], t48 = [4, 8], t49 = [4, 9],
   t56 = [5, 6], t57 = [5, 7], t58 = [5, 8], t59 = [5, 9],
   t62 = [6, 2], t64 = [6, 4], t67 = [6, 7], t68 = [6, 8],
@@ -28,7 +28,7 @@ const ruinedWall1 = [
 
 // create random underground texture
 const undergroundTiles = [
-  t62, t71, t72, t72, t72, t72, t72, t72, t72
+  t04, t14
 ]
 for (let i = -30; i < 158; i++) {
   fg.unshift({ x: i, y: 119, tile: random(undergroundTiles) })
@@ -36,7 +36,7 @@ for (let i = -30; i < 158; i++) {
 
 // create random grass
 const grassTiles = [
-  t41, t00, t00, t00, t00, t00
+  t01, t11, t01, t11, t01, t11, t01, t11, t26, t36
 ]
 for (let i = 0; i < 128; i++) {
   fg.unshift({ x: i, y: 118, tile: random(grassTiles) })
@@ -47,10 +47,11 @@ fg = fg.concat(parsePattern(ruinedWall1, 5, 117))
 export default {
   world: { w: 1000, h: 1024 },
   start: {
-    city: { x: 10, y: 1024 - constants.GROUNDHEIGHT - constants.MENU.h - 30}
+    city: { x: 20, y: 1024 - constants.GROUNDHEIGHT - constants.MENU.h - 30}
   },
   parallax: parallax.map(tile => mapTile(tile, tileSize)),
   bg: bg.map(tile => mapTile(tile, tileSize)),
   fg: fg.map(tile => mapTile(tile, tileSize)),
-  events
+  events,
+  track: 'santaMaria'
 }
