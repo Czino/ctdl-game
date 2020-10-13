a203665309f0c19eddec8291bdcd660fd5da318b
Czino <czino@Czino-MBP.home>
2020-10-13 23:01:42 +0200

    Add brian's theme

--- src/brian.js
+++ src/brian.js
@@ -6,6 +6,7 @@ import { write } from './font';
 import { addTextToQueue, setTextQueue } from './textUtils';
 import constants from './constants';
 import { playSound } from './sounds';
+import { initSoundtrack } from './soundtrack';
 
 const sprites = {
   brian
@@ -92,7 +93,7 @@ export default function(id, options) {
     })
     addTextToQueue('Brian:\nI should have stayed\nBitcoin only...')
     addTextToQueue(`Brian got rekt,\nyou found $${this.usd}`, () => {
-      // TODO change back to main theme
+      initSoundtrack(CTDLGAME.world.map.soundtrack)
       if (this.item) {
         let item = new Item(
           this.item.id,
@@ -183,7 +184,7 @@ export default function(id, options) {
       addTextToQueue('Brian:\nI will delete you!', () => {
         this.canMove = true
         CTDLGAME.lockCharacters = false
-        // TODO change to brian's theme
+        initSoundtrack('briansTheme')
       })
       this.hadIntro = true
     }
