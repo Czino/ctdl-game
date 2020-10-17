763017134cbdc29cd5e3fcfeea17768d65068fd8
Czino <eric@czino.de>
2020-10-17 19:30:48 +0200

    Align icons

--- src/sprites/icons.js
+++ src/sprites/icons.js
@@ -12,13 +12,13 @@ export default {
     "jump-hodlonaut": { x: 16, y: 16, w: 7, h: 16},
     "back-hodlonaut": { x: 24, y: 16, w: 7, h: 16},
     "duck-hodlonaut": { x: 31, y: 16, w: 7, h: 16},
-    "attack-hodlonaut": { x: 38, y: 16, w: 9, h: 16},
-    "switch-hodlonaut": { x: 50, y: 16, w: 10, h: 16},
+    "attack-hodlonaut": { x: 40, y: 16, w: 9, h: 16},
+    "switch-hodlonaut": { x: 51, y: 16, w: 10, h: 16},
     "right-katoshi": { x: 0, y: 33, w: 7, h: 16},
     "left-katoshi": { x: 8, y: 33, w: 7, h: 16},
     "jump-katoshi": { x: 16, y: 33, w: 7, h: 16},
     "back-katoshi": { x: 24, y: 33, w: 7, h: 16},
     "duck-katoshi": { x: 31, y: 33, w: 7, h: 16},
-    "attack-katoshi": { x: 38, y: 33, w: 9, h: 16},
-    "switch-katoshi": { x: 50, y: 33, w: 10, h: 16}
+    "attack-katoshi": { x: 40, y: 33, w: 9, h: 16},
+    "switch-katoshi": { x: 51, y: 33, w: 10, h: 16}
 }
