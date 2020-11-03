835c87b8511c42fed62271abd60ad629e3edc446
Czino <eric@czino.de>
2020-11-03 20:14:13 +0100

    Save wizard for a better stage

--- src/mapUtils/maps/city.js
+++ src/mapUtils/maps/city.js
@@ -287,13 +287,6 @@ export default {
       }
     ),
     new NPC(
-      'wizardWithNoMoney',
-      {
-        x: 247,
-        y: 1024 - constants.GROUNDHEIGHT - constants.MENU.h - 28
-      }
-    ),
-    new NPC(
       'mirco',
       {
         x: 287,
