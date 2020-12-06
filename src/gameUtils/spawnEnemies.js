78714ec30a8d6c2121311f554f09fac9e5863906
Czino <eric@czino.de>
2020-12-06 14:09:13 +0100

    Improve code for spawning enemies

--- src/gameUtils/spawnEnemies.js
+++ src/gameUtils/spawnEnemies.js
@@ -5,6 +5,36 @@ import Goldbugs from '../enemies/Goldbugs'
 import { CTDLGAME } from './CTDLGAME'
 import { intersects } from '../geometryUtils'
 
+
+/**
+ * @description Method to spawn agent
+ * @param {Agent} agent the agent to spawn
+ */
+export const spawnAgent = agent => {
+  // check vertical if solid tiles can be found
+  // TODO improve code to work in tunnels
+  let vertical = {
+    x: agent.getBoundingBox(),
+    y: CTDLGAME.viewport.y,
+    w: agent.getBoundingBox().w,
+    h: constants.HEIGHT
+  }
+  let highestSolid = CTDLGAME.quadTree.query(vertical)
+      .filter(point => point.isSolid)
+      .sort((a, b) => a.y > b.y ? 1 : -1)
+      .find(() => true)
+
+  if (highestSolid) {
+    agent.y = highestSolid.y - agent.getBoundingBox().h
+  }
+  let hasCollided = CTDLGAME.quadTree.query(agent.getBoundingBox())
+    .filter(point => point.isSolid && point.id !== agent.id)
+    .some(point => intersects(agent.getBoundingBox(), point.getBoundingBox()))
+
+  if (!hasCollided) {
+    CTDLGAME.objects.push(agent)
+  }
+}
 /**
  * @description Method that takes care of spawning enemies according to spawn rate and world
  * @returns {void}
@@ -12,78 +42,33 @@ import { intersects } from '../geometryUtils'
 export const spawnEnemies = () => {
   if (CTDLGAME.isNight && Math.random() < CTDLGAME.world.map.spawnRates.shitcoiner) {
     // TODO maybe consider iterating through worldObects
-    let shitcoiner = new Shitcoiner(
+    spawnAgent(new Shitcoiner(
       'shitcoiner-' + Math.random(),
       {
         x: CTDLGAME.viewport.x + Math.round(Math.random() * constants.WIDTH),
-        y: CTDLGAME.world.h - constants.GROUNDHEIGHT - constants.MENU.h - 30,
+        y: CTDLGAME.viewport.y + constants.HEIGHT - 1,
         status: 'spawn'
       }
-    )
-
-    // spawn low and test down until void is found
-    let spawned = false
-    do {
-      let hasCollided = CTDLGAME.quadTree.query(shitcoiner.getBoundingBox())
-        .filter(point => point.isSolid && point.id !== shitcoiner.id)
-        .some(point => intersects(shitcoiner.getBoundingBox(), point.getBoundingBox()))
-
-      if (!hasCollided) {
-        spawned = true
-        CTDLGAME.objects.push(shitcoiner)
-      } else {
-        shitcoiner.y -= 1
-      }
-    } while (!spawned && shitcoiner.y > 0)
+    ))
   }
   if (Math.random() < CTDLGAME.world.map.spawnRates.rabbit) {
-    let rabbit = new Rabbit(
+    spawnAgent(new Rabbit(
       'rabbit-' + Math.random(),
       {
         x: CTDLGAME.viewport.x + Math.round(Math.random() * constants.WIDTH),
-        y: CTDLGAME.viewport.y + constants.HEIGHT - constants.MENU.h,
+        y: CTDLGAME.viewport.y + constants.HEIGHT,
         status: 'spawn'
       }
-    )
-
-    // spawn low and test down until void is found
-    let spawned = false
-    do {
-      let hasCollided = CTDLGAME.quadTree.query(rabbit.getBoundingBox())
-        .filter(point => point.isSolid && point.id !== rabbit.id)
-        .some(point => intersects(rabbit.getBoundingBox(), point.getBoundingBox()))
-
-      if (!hasCollided) {
-        spawned = true
-        CTDLGAME.objects.push(rabbit)
-      } else {
-        rabbit.y -= 1
-      }
-    } while (!spawned && rabbit.y > 0)
+    ))
   }
   if (Math.random() < CTDLGAME.world.map.spawnRates.goldbugs) {
-    let goldbugs = new Goldbugs(
+    spawnAgent(new Goldbugs(
       'goldbugs-' + Math.random(),
       {
         x: CTDLGAME.viewport.x + Math.round(Math.random() * constants.WIDTH),
-        y: CTDLGAME.viewport.y + constants.HEIGHT - constants.MENU.h,
+        y: CTDLGAME.viewport.y + constants.HEIGHT,
         status: 'idle'
       }
-    )
-
-    // spawn low and test down until void is found
-    let spawned = false
-    do {
-      let hasCollided = CTDLGAME.quadTree.query(goldbugs.getBoundingBox())
-        .filter(point => point.isSolid && point.id !== goldbugs.id)
-        .some(point => intersects(goldbugs.getBoundingBox(), point.getBoundingBox()))
-
-      if (!hasCollided) {
-        spawned = true
-        CTDLGAME.objects.push(goldbugs)
-      } else {
-        goldbugs.y -= 1
-      }
-    } while (!spawned && goldbugs.y > 0)
+    ))
   }
 }
