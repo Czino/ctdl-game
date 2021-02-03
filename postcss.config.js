3521fe2169ea98d2b620959a903a78ab680b6175
Czino <eric@czino.de>
2021-02-03 18:47:16 +0100

    fix purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -3,9 +3,10 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 module.exports = {
   plugins: [
     require('tailwindcss'),
-    // purgecss({
-    //   content: ['./src/*.html']
-    // }),
+    purgecss({
+      content: ['./src/*.html'],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
     require('autoprefixer')
   ]
 }
