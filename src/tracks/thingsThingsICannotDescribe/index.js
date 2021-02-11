31d3f9d0bef908a6d9694b282cc6fd0781856a9a
Czino <eric@czino.de>
2021-02-11 19:07:19 +0100

    Add new soundtrack

---
+++ src/tracks/thingsThingsICannotDescribe/index.js
@@ -0,0 +1,22 @@
+// Remi - Things Things I cannot Describe (Forz Remix | Czino 8-bit Cover)
+
+import triangle from './triangle'
+import sine from './sine'
+import square from './square'
+import noise from './noise'
+import brownNoise from './brownNoise'
+import drum from './drum'
+
+export default {
+  id: 'thingsThingsICannotDescribe',
+  length: 241.491,
+  loop: true,
+  tracks: {
+    triangle,
+    sine,
+    square,
+    noise,
+    brownNoise,
+    drum
+  }
+}
