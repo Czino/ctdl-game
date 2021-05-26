2fecae8d8a8eb3df200d38f302b2f2ba270d44d9
Czino <eric@czino.de>
2021-05-26 17:24:00 +0200

    Bugfix burning shitcoiners

--- src/gameUtils/cleanUpStage.js
+++ src/gameUtils/cleanUpStage.js
@@ -13,16 +13,17 @@ export const cleanUpStage = () => {
     .filter(obj => obj && !obj.remove && obj.y < 2048) // remove objects that are marked for removal
 
   CTDLGAME.objects = CTDLGAME.objects.filter(obj => {
+    if (obj.getClass() === 'Shitcoiner' && obj.status === 'rekt' && !CTDLGAME.isNight) obj.status = 'burning'
     if (obj.removeTimer) obj.removeTimer--
     if (obj.removeTimer === 0) return false
 
-    if (obj.class === 'Shitcoiner' && !CTDLGAME.isNight && obj.status === 'burning' && Math.random() < .25) {
+
+    if (obj.getClass() === 'Shitcoiner' && !CTDLGAME.isNight && obj.status === 'burning' && Math.random() < .25) {
       return false
     }
 
     if (obj.status !== 'rekt' && obj.status !== 'burning') return true
 
-    if (obj.class === 'Shitcoiner' && !CTDLGAME.isNight) obj.status = 'burning'
     return true
   })
 }
