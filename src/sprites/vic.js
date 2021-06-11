b875eaa03f14e7f784b1bb1b263e8a4ff60ffcaf
Czino <eric@czino.de>
2021-06-11 20:32:53 +0200

    Add Vic

---
+++ src/sprites/vic.js
@@ -0,0 +1,38 @@
+const w = 23
+const h = 55
+
+export default {
+  right: {
+    idle: [
+      { x: 0 * w, y: 0 * h, w, h },
+      { x: 0 * w, y: 0 * h, w, h },
+      { x: 0 * w, y: 0 * h, w, h },
+      { x: 1 * w, y: 0 * h, w, h },
+      { x: 2 * w, y: 0 * h, w, h },
+      { x: 3 * w, y: 0 * h, w, h },
+      { x: 4 * w, y: 0 * h, w, h },
+      { x: 5 * w, y: 0 * h, w, h },
+      { x: 5 * w, y: 0 * h, w, h },
+      { x: 5 * w, y: 0 * h, w, h },
+      { x: 3 * w, y: 0 * h, w, h },
+      { x: 2 * w, y: 0 * h, w, h },
+      { x: 1 * w, y: 0 * h, w, h },
+    ],
+    happy: [
+      { x: 0 * w, y: 1 * h, w, h },
+      { x: 1 * w, y: 1 * h, w, h },
+      { x: 2 * w, y: 1 * h, w, h },
+      { x: 3 * w, y: 1 * h, w, h },
+      { x: 4 * w, y: 1 * h, w, h },
+      { x: 3 * w, y: 1 * h, w, h },
+      { x: 2 * w, y: 1 * h, w, h },
+      { x: 2 * w, y: 1 * h, w, h },
+      { x: 2 * w, y: 1 * h, w, h },
+      { x: 2 * w, y: 1 * h, w, h },
+      { x: 2 * w, y: 1 * h, w, h },
+      { x: 2 * w, y: 1 * h, w, h },
+      { x: 2 * w, y: 1 * h, w, h },
+      { x: 1 * w, y: 1 * h, w, h },
+    ]
+  }
+}
